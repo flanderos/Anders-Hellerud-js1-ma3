82d84f655fd4501894aafa8caf0ba0e0898ff03c
flanderos <andershellerud@hotmail.no>
2022-05-14 21:08:50 +0200

    first one works!

---
+++ question-1.js
@@ -0,0 +1,11 @@
+//Regular Function
+
+function getRemainder(a, b) {
+  return a % b;
+}
+
+//Arrow Function
+
+getRemainder = (a, b) => {
+  return a % b;
+};
---
+++ question-2.js
@@ -0,0 +1,25 @@
+const container = document.querySelector(".container");
+const gameName = document.querySelector(".name");
+const gameRating = document.querySelector(".rating");
+const gameTags = document.querySelector(".tags");
+
+const url =
+  "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=3533b7792e1b452baa8d22576f4b65b0";
+
+async function getName() {
+  const response = await fetch(url);
+
+  const results = await response.json();
+
+  const gameList = results.results;
+
+  for (let i = 0; i < gameList.length; i++) {
+    if (i > 7) {
+      break;
+    }
+    gameName.innerHTML += `<li>${gameList[i].name}</li>`;
+    console.log(gameList[i].name);
+  }
+}
+
+getName();
